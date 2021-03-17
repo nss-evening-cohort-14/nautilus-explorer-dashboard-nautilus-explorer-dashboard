87418ce0a3205ad6f665f88d29f0c06980fd0c59
Honey-Rae Swan <honeyraeswan@gmail.com>
2021-03-16 19:40:52 -0500

    added navbar and media with styling

---
+++ src/javascripts/components/navBar.js
@@ -0,0 +1,44 @@
+import crewOptionTwo from '../../assets/crewOptionTwo.png';
+import destination from '../../assets/destination.png';
+import excursions from '../../assets/excursions.png';
+import krakenIcon from '../../assets/krakenIcon.png';
+import log from '../../assets/log.png';
+import logoOnly from '../../assets/logoOnly.png';
+import environmentalVariables from '../../assets/environmentalVariables.png';
+
+const navBar = () => {
+  document.querySelector('#app').innerHTML = `<nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="#"><img src=${logoOnly}></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="readCrew"><img src=${crewOptionTwo}></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="#" id="readDestinations"><img src=${destination}></a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#" id="readLogEntries"><img src=${log}></a>
+  </li>
+  <li class="nav-item">
+  <a class="nav-link" href="#" id="readEnvironmentalVariables"><img src=${environmentalVariables}></a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#" id="readSpecies"><img src=${krakenIcon}></a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#" id="readExcursions"><img src=${excursions}></a>
+</li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn" type="submit" id="loginLogoutButton"></button>
+    </form>
+  </div>
+</nav>`;
+};
+
+export default navBar;
--- src/javascripts/helpers/router.js
+++ src/javascripts/helpers/router.js
@@ -1,17 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import loginButton from '../components/loginButton';
-import logoutButton from '../components/logoutButton';
-import firebaseConfig from './apiKeys';
+import loginButton from '../components/buttons/loginButton';
+import startApp from '../views/startApp';
+import firebaseConfig from './auth/apiKeys';
 
 const checkLoginStatus = () => {
   firebase.initializeApp(firebaseConfig);
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      // person is logged in do something...
-      logoutButton();
+      startApp(user.uid);
     } else {
-      // person is NOT logged in
       loginButton();
     }
   });
