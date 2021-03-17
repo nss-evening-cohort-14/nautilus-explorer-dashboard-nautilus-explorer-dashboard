2ef1cb9f5dab38667690f4da4431fc76a3c92611
HunterJuneau <huntlee17@gmail.com>
2021-03-16 21:38:05 -0500

    Did more styling.

--- src/javascripts/views/_dashboardView.js
+++ src/javascripts/views/_dashboardView.js
@@ -1,35 +1,45 @@
 const dashboardView = (user) => {
   $('#page').html(`<h1>Nautilus Explorer Dashboard</h1>
                   ${user ? `<h2>Welcome Aboard, Captain!</h2>` : ''}
-                  <div class="d-flex flex-wrap justify-content-around" id="pageCardsContainer">
-                    <div class="card dashboard-card">
-                      <div class="card-body">
-                        <h3 class="card-title">Crew Members</h3>
-                        <button type="button" class="btn btn-primary">Go there!</button>
+                  <div id="pageCardsContainer">
+                    <div class="width-50">
+                      <div class="card dashboard-card">
+                        <div class="card-body">
+                          <h3 class="card-title">Crew Members</h3>
+                          <button type="button" class="btn btn-primary">Go there!</button>
+                        </div>
                       </div>
                     </div>
-                    <div class="card dashboard-card">
-                      <div class="card-body">
-                        <h3 class="card-title">Destinations</h3>
-                        <button type="button" class="btn btn-primary">Go there!</button>
+                    <div class="width-50">
+                      <div class="card dashboard-card">
+                        <div class="card-body">
+                          <h3 class="card-title">Destinations</h3>
+                          <button type="button" class="btn btn-primary">Go there!</button>
+                        </div>
                       </div>
                     </div>
-                    <div class="card dashboard-card">
-                      <div class="card-body">
-                        <h3 class="card-title">Log Entries</h3>
-                        <button type="button" class="btn btn-primary">Go there!</button>
+                    <div class="width-50">
+                      <div class="card dashboard-card">
+                        <div class="card-body">
+                          <h3 class="card-title">Log Entries</h3>
+                          <button type="button" class="btn btn-primary">Go there!</button>
+                        </div>
                       </div>
                     </div>
-                    <div class="card dashboard-card">
-                      <div class="card-body">
-                        <h3 class="card-title">Environmental Variables</h3>
-                        <button type="button" class="btn btn-primary">Go there!</button>
+                    <div class="width-50">
+                      <div class="card dashboard-card">
+                        <div class="card-body">
+                          <h3 class="card-title">Environmental Variables</h3>
+                          <button type="button" class="btn btn-primary">Go there!</button>
+                        </div>
                       </div>
                     </div>
-                    <div class="card dashboard-card">
-                      <div class="card-body">
-                        <h3 class="card-title">Species</h3>
-                        <button type="button" class="btn btn-primary">Go there!</button>
+                    <div class="width-100">
+                      <div class="card dashboard-card">
+                        <div class="card-body">
+                          <h3 class="card-title">Species</h3>
+                          <button type="button" class="btn btn-primary">Go there!</button>
+                        </div>
                       </div>
                     </div>
                   </div>
