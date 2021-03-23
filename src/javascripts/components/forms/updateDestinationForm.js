53235cabb0dd40e8bf9c01d08455f8a2f9a3c9dc
HunterJuneau <huntlee17@gmail.com>
2021-03-22 21:15:52 -0500

    Improved styling of Edit Form.

--- src/javascripts/components/forms/updateDestinationForm.js
+++ src/javascripts/components/forms/updateDestinationForm.js
@@ -2,8 +2,8 @@ const updateDestinationForm = (destinationObject) => {
   $('#addButton').html('');
   $('#cardContainer').html('');
 
-  $('#formContainer').html(`<h1>Edit ${destinationObject.name}</h2>
-                            <form id="updateDestinationForm--${destinationObject.firebaseKey}">
+  $('#formContainer').html(`<h1 class="text-center">Edit ${destinationObject.name}</h2>
+                            <form class="w-75 mx-auto" id="updateDestinationForm--${destinationObject.firebaseKey}">
                               <div class="mb-3">
                                 <label for="destinationName" class="form-label">Name</label>
                                 <input type="text" class="form-control" id="destinationName" required>
@@ -12,8 +12,7 @@ const updateDestinationForm = (destinationObject) => {
                                 <label for="destinationImage" class="form-label">Image URL</label>
                                 <input type="url" class="form-control" id="destinationImage" required>
                               </div>
-                              <button type="submit" class="btn btn-primary">Add Destination</button>
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                              <button type="submit" class="btn btn-primary">Confirm Changes</button>
                             </form>`);
 
   $('#destinationName').val(destinationObject.name);
